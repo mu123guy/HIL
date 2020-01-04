8339e66a49af0dcebd4c165dd006929bf79d71e3
Guy <Guy-m@campus.technion.ac.il>
2020-01-04 20:50:14 +0200

    HD version of flight gear for use with discrete card

--- GRM_HIL_Simulation/runfg HD.bat
+++ GRM_HIL_Simulation/runfg HD.bat
@@ -3,4 +3,4 @@ SET FG_ROOT=C:\Program Files\FlightGear\data
 SET FG_SCENERY=C:\Program Files\FlightGear\data\Scenery;C:\Program Files\FlightGear\scenery;C:\Program Files\FlightGear\terrasync"
 
 
-"C:\Program Files\FlightGear\bin\fgfs" --aircraft=HL20 --fdm=null --enable-auto-coordination --native-fdm=socket,in,30,localhost,5502,udp --start-date-lat=2004:06:01:19:00:00 --visibility=15000 --disable-freeze --airport=TLV --runway=08L --heading=0 --offset-distance=0 --offset-azimuth=0 --enable-rembrandt 
+"C:\Program Files\FlightGear\bin\fgfs" --aircraft=HL20 --fdm=null --enable-auto-coordination --native-fdm=socket,in,30,localhost,5502,udp --start-date-lat=2004:06:01:19:00:00 --visibility=15000 --disable-freeze --disable-clouds --airport=TLV --runway=08L --heading=0 --offset-distance=0 --offset-azimuth=0 --enable-rembrandt 
